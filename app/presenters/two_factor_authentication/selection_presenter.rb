f52190d94e9222f80fa0036d40015a7e17d379eb
Alison Dowski <alison.dowski@gsa.gov>
2019-06-25 10:17:10 -0400

    added learn more links

--- app/presenters/two_factor_authentication/selection_presenter.rb
+++ app/presenters/two_factor_authentication/selection_presenter.rb
@@ -2,6 +2,7 @@ module TwoFactorAuthentication
   class SelectionPresenter
     include Rails.application.routes.url_helpers
     include ActionView::Helpers::TranslationHelper
+    include ActionView::Helpers::UrlHelper
 
     attr_reader :configuration
 
@@ -29,6 +30,14 @@ module TwoFactorAuthentication
       ''
     end
 
+    def learn_more?
+      (type == "webauthn" || type == "auth_app") && option_mode.to_s == "two_factor_choice_options"
+    end
+
+    def learn_more_path
+      type == "webauthn" ? webauthn_learn_more_path : auth_app_learn_more_path
+    end
+
     private
 
     def option_mode
@@ -37,6 +46,14 @@ module TwoFactorAuthentication
       else
         'two_factor_choice_options'
       end
+    end
+
+    def auth_app_learn_more_path
+      'https://login.gov/help/signing-in/what-is-an-authentication-app/'
+    end
+
+    def webauthn_learn_more_path
+      'https://login.gov/help/security-keys/what-is-a-security-key/'
     end
   end
 end
